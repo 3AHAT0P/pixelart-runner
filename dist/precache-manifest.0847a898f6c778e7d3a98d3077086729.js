3c4463f31336f81e12b1018d86216c33fb882e96
Igor Kostyakov <i.a.kostyakov@gmail.com>
2019-05-11 21:32:27 +0300

    Release 0.1

---
+++ dist/precache-manifest.0847a898f6c778e7d3a98d3077086729.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    revision: '997e6d1a3c68ba14ff593340760fd8c5',
+    url: '/img/runner/runner-run-left-0.png',
+  },
+  {
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: '/robots.txt',
+  },
+  {
+    revision: '072b7543b81d3a509be7',
+    url: '/js/chunk-vendors.3fd363ad.js',
+  },
+  {
+    revision: '87ce6c89e58c40688676',
+    url: '/js/app.e277dc84.js',
+  },
+  {
+    revision: '47c065b556ed88499aa3fd9435bb883c',
+    url: '/index.html',
+  },
+  {
+    revision: '2350da240e9fa3147842c14f97fe18b7',
+    url: '/img/runner/runner-standing-left.png',
+  },
+  {
+    revision: '72d9134228eef0f13d409ebac27464d7',
+    url: '/img/runner/runner-standing-left-closed-eyes.png',
+  },
+  {
+    revision: '9f60421addb8c3afe8c67f077da12ae8',
+    url: '/img/runner/runner-run-left-2.png',
+  },
+  {
+    revision: '34026d6cb135724adcaf7308a8612c94',
+    url: '/img/runner/runner-run-left-1.png',
+  },
+  {
+    revision: '2b8db9f35755a4a2d06ba7eb5e0effe3',
+    url: '/img/runner/runner-jumping-left.png',
+  },
+  {
+    revision: '87ce6c89e58c40688676',
+    url: '/css/app.ad3fd43c.css',
+  },
+];
