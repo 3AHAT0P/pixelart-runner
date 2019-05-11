a6f7b61b504a886db6456ceee5783307029eac52
Igor Kostyakov <i.a.kostyakov@gmail.com>
2019-05-11 22:11:49 +0300

    Fix bug with public path

--- precache-manifest.1bab8c81be3b92bdd9aed22ed424c4eb.js
+++ precache-manifest.1bab8c81be3b92bdd9aed22ed424c4eb.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
     "revision": "997e6d1a3c68ba14ff593340760fd8c5",
-    "url": "/img/runner/runner-run-left-0.png"
+    "url": "/pixelart-runner/img/runner/runner-run-left-0.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
@@ -21,23 +21,23 @@ self.__precacheManifest = [
   },
   {
     "revision": "2350da240e9fa3147842c14f97fe18b7",
-    "url": "/img/runner/runner-standing-left.png"
+    "url": "/pixelart-runner/img/runner/runner-standing-left.png"
   },
   {
     "revision": "72d9134228eef0f13d409ebac27464d7",
-    "url": "/img/runner/runner-standing-left-closed-eyes.png"
+    "url": "/pixelart-runner/img/runner/runner-standing-left-closed-eyes.png"
   },
   {
     "revision": "9f60421addb8c3afe8c67f077da12ae8",
-    "url": "/img/runner/runner-run-left-2.png"
+    "url": "/pixelart-runner/img/runner/runner-run-left-2.png"
   },
   {
     "revision": "34026d6cb135724adcaf7308a8612c94",
-    "url": "/img/runner/runner-run-left-1.png"
+    "url": "/pixelart-runner/img/runner/runner-run-left-1.png"
   },
   {
     "revision": "2b8db9f35755a4a2d06ba7eb5e0effe3",
-    "url": "/img/runner/runner-jumping-left.png"
+    "url": "/pixelart-runner/img/runner/runner-jumping-left.png"
   },
   {
     "revision": "ee93df6dbb1e073a1489",
